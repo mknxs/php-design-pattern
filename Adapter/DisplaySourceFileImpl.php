2db4f038f925301b5a63e2c203758e6b3d616531
smakino <re.verage.lalala@gmail.com>
2014-07-06 13:56:40 +0900

    addapter pattern

---
+++ Adapter/DisplaySourceFileImpl.php
@@ -0,0 +1,16 @@
+<?php
+require_once "DisplaySourceFile.php";
+require_once "ShowFile.php";
+
+class DisplaySourceFileImpl extends ShowFile implements DisplaySourceFile {
+
+  public function __construct($filename) {
+    parent::__construct($filename);
+  }
+
+  public function display() {
+    parent::showHighlight();
+  }
+}
+
+?>
